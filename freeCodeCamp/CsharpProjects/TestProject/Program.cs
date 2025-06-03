6f7d484a9f5c2ccef23ad8ce19cc5f058e830d8c
Vinicius Lopes <vinicius.lopes17@hotmail.com>
2025-06-03 16:56:09 -0300

    First commit

---
+++ freeCodeCamp/CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,238 @@
+﻿// Console.WriteLine("Signed integral types:");
+
+// Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+// Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+// Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+// Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+// Console.WriteLine("");
+// Console.WriteLine("Unsigned integral types:");
+
+// Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+// Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+// Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+// Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+// Console.WriteLine("");
+// Console.WriteLine("Floating point types:");
+// Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+// Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+// Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+
+// int[] data = new int[3];
+
+// string shortenedString = "Hello World!";
+// Console.WriteLine(shortenedString);
+
+// int val_A = 2;
+// int val_B = val_A;
+// val_B = 5;
+
+// Console.WriteLine("--Value Types--");
+// Console.WriteLine($"val_A: {val_A}");
+// Console.WriteLine($"val_B: {val_B}");
+
+// int[] ref_A= new int[1];
+// ref_A[0] = 2;
+// int[] ref_B = ref_A;
+// ref_B[0] = 5;
+
+// Console.WriteLine("--Reference Types--");
+// Console.WriteLine($"ref_A[0]: {ref_A[0]}");
+// Console.WriteLine($"ref_B[0]: {ref_B[0]}");
+
+// int first = 2;
+// string second = "4";
+// int result = first + second;
+// Console.WriteLine(result);
+
+// int first = 2;
+// string second = "4";
+// string result = first + second;
+// Console.WriteLine(result);
+
+// int myInt = 3;
+// Console.WriteLine($"int: {myInt}");
+
+// decimal myDecimal = myInt;
+// Console.WriteLine($"decimal: {myDecimal}");
+
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
+
+// int myInt = (int)myDecimal;
+// Console.WriteLine($"int: {myInt}");
+
+// decimal myDecimal = 1.23456789m;
+// float myFloat = (float)myDecimal;
+
+// Console.WriteLine($"Decimal: {myDecimal}");
+// Console.WriteLine($"Float  : {myFloat}");
+
+// int first = 5;
+// int second = 7;
+// string message = first.ToString() + second.ToString();
+// Console.WriteLine(message);
+
+// string first = "5";
+// string second = "7";
+// int sum = int.Parse(first) + int.Parse(second);
+// Console.WriteLine(sum);
+
+// string value1 = "5";
+// string value2 = "7";
+// int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+// Console.WriteLine(result);
+
+// int value = (int)1.5m; // casting truncates
+// Console.WriteLine(value);
+
+// int value2 = Convert.ToInt32(1.5m); // converting rounds up
+// Console.WriteLine(value2);
+
+// string name = "Bob";
+// int tent = 0;
+// Console.WriteLine(name.TryParse(name, out tent));
+#region challengeConversion
+// int value1 = 11;
+// decimal value2 = 6.2m;
+// float value3 = 4.3f;
+
+// int result1 = Convert.ToInt32(value1 / value2); 
+// // Hint: You need to round the result to nearest integer (don't just truncate)
+// Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+// decimal result2 = value2 / Convert.ToDecimal(value3);
+// Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+// float result3 = value3 / value1;
+// Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+#endregion
+
+// string[] pallets = [ "B14", "A11", "B12", "A13" ];
+
+// Console.WriteLine("Sorted...");
+// Array.Sort(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Console.WriteLine("Reversed...");
+// Array.Reverse(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string[] pallets = [ "B14", "A11", "B12", "A13" ];
+// Console.WriteLine("");
+
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 6);
+// Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+// pallets[4] = "C01";
+// pallets[5] = "C02";
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 3);
+// Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string value = "abc123";
+// char[] valueArray = value.ToCharArray();
+// Array.Reverse(valueArray);
+// // string result = new string(valueArray);
+// string result = String.Join(", ", valueArray);
+// Console.WriteLine(result);
+
+// string[] items = result.Split(',');
+// foreach (string item in items)
+// {
+//     Console.WriteLine(item);
+// }
+#region challangeSortArray
+// string pangram = "The quick brown fox jumps over the lazy dog";
+// // Step 1
+// string[] message = pangram.Split(' ');
+// //Step 2
+// string[] newMessage = new string[message.Length];
+// // Step 3
+// for (int i = 0; i < message.Length; i++)
+// {
+//     char[] letters = message[i].ToCharArray();
+//     Array.Reverse(letters);
+//     newMessage[i] = new string(letters);
+// }
+
+// //Step 4
+// string result = String.Join(" ", newMessage);
+// Console.WriteLine(result);
+#endregion
+
+#region OrderArray
+// string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+// string[] items = orderStream.Split(',');
+// Array.Sort(items);
+
+// foreach (var item in items)
+// {
+//     if (item.Length == 4)
+//     {
+//         Console.WriteLine(item);
+//     }
+//     else
+//     {
+//         Console.WriteLine(item + "\t- Error");
+//     }
+// }
+#endregion
+
+#region formatStrings
+
+// string first = "Hello";
+// string second = "World";
+// string last = "My Last Name is ...";
+// string result = string.Format("{0} {1}! {2}", first, second, last);
+// Console.WriteLine(result);
+
+// string first = "Hello";
+// string second = "World";
+// Console.WriteLine($"{first} {second}!");
+// Console.WriteLine($"{second} {first}!");
+// Console.WriteLine($"{first} {first} {first}!");
+
+// decimal price = 123.45m;
+// int discount = 50;
+// Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+// decimal measurement = 123456.78912m;
+// Console.WriteLine($"Measurement: {measurement:N} units");
+// If you want to display more precision, you can do that by adding a number after the specifier. 
+// The following code will display four digits after the decimal point using the N4 specifier. Update your code as follows:
+// decimal measurement = 123456.78912m;
+// Console.WriteLine($"Measurement: {measurement:N4} units");
+
+// Use the P format specifier to format percentages and rounds to 2 decimal places. 
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P2}");
+
+#endregion
