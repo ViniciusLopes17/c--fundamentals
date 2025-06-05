c69e3cbbd710864c5ee2f2a94bfe248c43820be3
Vinicius Lopes <vinicius.lopes17@hotmail.com>
2025-06-05 17:37:58 -0300

    updates

--- freeCodeCamp/CsharpProjects/TestProject/Program.cs
+++ freeCodeCamp/CsharpProjects/TestProject/Program.cs
@@ -220,19 +220,355 @@
 // Console.WriteLine($"{second} {first}!");
 // Console.WriteLine($"{first} {first} {first}!");
 
+// ---- the :C currency format specifier is used to present the price and discount variables as currency. Update your code as follows:
 // decimal price = 123.45m;
 // int discount = 50;
 // Console.WriteLine($"Price: {price:C} (Save {discount:C})");
 
+//------ The N numeric format specifier makes numbers more readable. Update your code as follows:
 // decimal measurement = 123456.78912m;
 // Console.WriteLine($"Measurement: {measurement:N} units");
+
 // If you want to display more precision, you can do that by adding a number after the specifier. 
 // The following code will display four digits after the decimal point using the N4 specifier. Update your code as follows:
 // decimal measurement = 123456.78912m;
 // Console.WriteLine($"Measurement: {measurement:N4} units");
 
 // Use the P format specifier to format percentages and rounds to 2 decimal places. 
-decimal tax = .36785m;
-Console.WriteLine($"Tax rate: {tax:P2}");
+// decimal tax = .36785m;
+// Console.WriteLine($"Tax rate: {tax:P2}");
+
+// decimal price = 67.55m;
+// decimal salePrice = 59.99m;
+
+// string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+// yourDiscount += $"A discount of {(price - salePrice)/price:P2}!";
+// Console.WriteLine(yourDiscount);
+
+// decimal tax = 0.12051m;
+// Console.WriteLine($"{tax:P1}");
+
+#endregion
+
+#region ExploreStringInterpolation
+// int invoiceNumber = 1201;
+// decimal productShares = 25.4568m;
+// decimal subtotal = 2750.00m;
+// decimal taxPercentage = .15825m;
+// decimal total = 3185.19m;
+
+// Console.WriteLine($"Invoice Number: {invoiceNumber}");
+// Console.WriteLine($"   Shares: {productShares:N2} Product");
+// Console.WriteLine($"     Sub Total: {subtotal:C}");
+// Console.WriteLine($"           Tax: {taxPercentage:P2}");
+// Console.WriteLine($"     Total Billed: {total:C}");
+#endregion
+
+#region DiscoverPaddingAndAlignment
+// string first = "Hello";
+// string second = "World";
+// string result = string.Format("{0} {1}!", first, second);
+// Console.WriteLine(result);
+
+// string input = "Pad this";
+// Console.WriteLine(input.PadLeft(12, '-'));
+//  Console.WriteLine(input.PadRight(12, '-'));
+
+// string paymentId = "769C";
+// string payeeName = "Mr. Stephen Ortega";
+// string paymentAmount = "$5,000.00";
+// string m = "Learning C#";
+
+// var formattedM = m.PadLeft(12);
+// var mLenght = m.Length;
+// var formattedLine = paymentId.PadRight(6);
+// formattedLine += payeeName.PadRight(24);
+// formattedLine += paymentAmount.PadLeft(10);
+
+// Console.WriteLine(formattedLine);
+// Console.WriteLine(formattedM);
+// Console.WriteLine(mLenght);
+// Console.WriteLine("1234567890123456789012345678901234567890");
+#endregion
+
+#region ChallangeStringInterpolation
+
+// string customerName = "Ms. Barros";
+
+// string currentProduct = "Magic Yield";
+// int currentShares = 2975000;
+// decimal currentReturn = 0.1275m;
+// decimal currentProfit = 55000000.0m;
+
+// string newProduct = "Glorious Future";
+// decimal newReturn = 0.13125m;
+// decimal newProfit = 63000000.0m;
+
+// // Your logic here
+// string firstMessage = $"Dear {customerName},\n";
+// firstMessage += $"As a customer of our {currentProduct} offering we are excited "; 
+// firstMessage += "to tell you about a new financial product that would dramatically increase your return.\n";
+// string secondMessage = $"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.\n";
+// string thirdMessage = $"Our new product, {newProduct} offers a return of {newReturn:P2}. ";
+// thirdMessage += $"Given your current volume, your potential profit would be {newProfit:C2}.\n";
+// Console.WriteLine(firstMessage.PadRight(12));
+// Console.WriteLine(secondMessage);
+// Console.WriteLine(thirdMessage);
+
+// Console.WriteLine("Here's a quick comparison:\n");
+
+// string comparisonMessage = "";
+
+// // Your logic here
+// comparisonMessage = currentProduct.PadRight(20);
+// comparisonMessage += String.Format("{0:P2}", currentReturn).PadRight(10);
+// comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+// comparisonMessage += "\n";
+// comparisonMessage += newProduct.PadRight(20);
+// comparisonMessage += String.Format("{0:P2}", newReturn).PadRight(10);
+// comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
+// Console.WriteLine(comparisonMessage);
+
+#endregion
+
+#region IndexOfAndSubstring
+
+// string message = "Find what is (inside the parentheses)";
+
+// int openingPosition = message.IndexOf('(');
+// int closingPosition = message.IndexOf(')');
+
+// // Console.WriteLine(openingPosition);
+// // Console.WriteLine(closingPosition);
+
+// openingPosition += 1;
+
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
+// ------------------------------------------------------------------------------
+// string message = "What is the value <span>between the tags</span>?";
+
+// int openingPosition = message.IndexOf("<span>");
+// int closingPosition = message.IndexOf("</span>");
+
+// openingPosition += 6;
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(length);
+// Console.WriteLine(message.Substring(openingPosition, length));
+
+
+// string message = "What is the value <span>between the tags</span>?";
+
+// const string openSpan = "<span>";
+// const string closeSpan = "</span>";
+
+// int openingPosition = message.IndexOf(openSpan);
+// int closingPosition = message.IndexOf(closeSpan);
+
+// openingPosition += openSpan.Length;
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
+
+#endregion
+
+#region IndexOfAndLastIndexOf
+
+// The .IndexOf() method returns the index of the first occurrence of a specified character or substring within a given string. 
+// The method .LastIndexOf() returns the index position of the last occurrence of a character or string within a given string. 
+// Both the Indexof() and LastIndexOf() methods return -1 if the character or string is not found.
+
+// string message = "hello there!";
+
+// int first_h = message.IndexOf('h');
+// int last_h = message.LastIndexOf('h');
+
+// Console.WriteLine($"For the message: '{message}', the first 'h' is at position {first_h} and the last 'h' is at position {last_h}.");
+
+// --------------------------------------------------------------------------------------------------------------------------------
+
+// string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+// int openingPosition = message.LastIndexOf('(');
+// openingPosition += 1;
+// // Console.WriteLine(openingPosition);
+// int closingPosition = message.LastIndexOf(')');
+// // Console.WriteLine(closingPosition);
+// int length = closingPosition - openingPosition;
+// // Console.WriteLine(length);
+// Console.WriteLine(message.Substring(openingPosition, length));
+
+// ---------------------------------------------------------------------------------------------------------------------------------
+
+// string message = "(What if) there are (more than) one (set of parentheses)?";
+// while (true)
+// {
+//     int openingPosition = message.IndexOf('(');
+//     if (openingPosition == -1) break;
+
+//     openingPosition += 1;
+//     int closingPosition = message.IndexOf(')');
+//     int length = closingPosition - openingPosition;
+//     // length++;
+//     Console.WriteLine(message.Substring(openingPosition, length));
+
+//     // Note the overload of the Substring to return only the remaining 
+//     // unprocessed message:
+//     message = message.Substring(closingPosition + 1);
+// }
+
+// ---------------------------------------------------------------------------------------------------------------------------------
+
+// string message = "Hello, world!";
+// char[] charsToFind = { 'a', 'e', 'i', 'o' };
+
+// int index = message.IndexOfAny(charsToFind);
+
+// Console.WriteLine($"Found '{message[index]}' in '{message}' at index: {index}.");
+
+// string message = "Help (find) the {opening symbols}";
+// Console.WriteLine($"Searching THIS Message: {message}");
+// char[] openSymbols = ['[', '{', '('];
+// int startPosition = 5;
+// int openingPosition = message.IndexOfAny(openSymbols);
+// Console.WriteLine($"Found WITHOUT using startPosition: {message.Substring(openingPosition)}");
+
+// openingPosition = message.IndexOfAny(openSymbols, startPosition);
+// Console.WriteLine($"Found WITH using startPosition {startPosition}:  {message.Substring(openingPosition)}");
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+
+// // The IndexOfAny() helper method requires a char array of characters. 
+// // You want to look for:
+
+// char[] openSymbols = ['[', '{', '('];
+
+// // You'll use a slightly different technique for iterating through 
+// // the characters in the string. This time, use the closing 
+// // position of the previous iteration as the starting index for the 
+// //next open symbol. So, you need to initialize the closingPosition 
+// // variable to zero:
+
+// int closingPosition = 0;
+
+// while (true)
+// {
+//     int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+
+//     if (openingPosition == -1) break;
+
+//     string currentSymbol = message.Substring(openingPosition, 1);
+
+//     // Now  find the matching closing symbol
+//     char matchingSymbol = ' ';
+
+//     switch (currentSymbol)
+//     {
+//         case "[":
+//             matchingSymbol = ']';
+//             break;
+//         case "{":
+//             matchingSymbol = '}';
+//             break;
+//         case "(":
+//             matchingSymbol = ')';
+//             break;
+//     }
+
+//     // To find the closingPosition, use an overload of the IndexOf method to specify 
+//     // that the search for the matchingSymbol should start at the openingPosition in the string. 
+
+//     openingPosition += 1;
+//     closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
+//     // Finally, use the techniques you've already learned to display the sub-string:
+
+//     int length = closingPosition - openingPosition;
+//     Console.WriteLine(message.Substring(openingPosition, length));
+// }
+
+#endregion
+
+#region RemoveReplace
+// The Remove() method works similarly to the Substring() method. You supply a starting position and the length to remove those characters from the string.
+// string data = "12345John Smith          5000  3  ";
+// string updatedData = data.Remove(5, 20);
+// Console.WriteLine(updatedData);
+
+// The Replace() method swaps all instances of a string with a new string.
+// string message = "This--is--ex-amp-le--da-ta";
+// message = message.Replace("--", " ");
+// message = message.Replace("-", "");
+// Console.WriteLine(message);
+#endregion
+
+#region ChallangeExtractReplaceRemove
+// My Code
+/*const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "Quantidade: ";
+string output = "Output: ";
+
+// Your work here
+Console.WriteLine(input.Length);
+
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+const string outputDiv = "<div>";
+const string outDiv = "</div>";
+
+int openingPosition = input.IndexOf(openSpan);
+int closingPosition = input.IndexOf(closeSpan);
+int openPos = input.IndexOf(outputDiv);
+int endPos = input.IndexOf(outDiv);
+
+openingPosition += openSpan.Length;
+int length = closingPosition - openingPosition;
+quantity += input.Substring(openingPosition, length);
+
+openPos += outputDiv.Length;
+int length2 = endPos - openPos;
+output += input.Substring(openPos, length2);
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);*/
+
+// Code Solved
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+
+// Extract the "quantity"
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int quantityStart = input.IndexOf(openSpan) + openSpan.Length; // + length of <span> so index at end of <span> tag
+int quantityEnd= input.IndexOf(closeSpan);
+int quantityLength = quantityEnd - quantityStart;
+quantity = input.Substring(quantityStart, quantityLength);
+quantity = $"Quantity: {quantity}";
+
+// Set output to input, replacing the trademark symbol with the registered trademark symbol
+const string tradeSymbol = "&trade;";
+const string regSymbol = "&reg;";
+output = input.Replace(tradeSymbol, regSymbol);
+
+// Remove the opening <div> tag
+const string openDiv = "<div>";
+int divStart = output.IndexOf(openDiv);
+output = output.Remove(divStart, openDiv.Length);
+
+// Remove the closing </div> tag and add "Output:" to the beginning
+const string closeDiv = "</div>";
+int divCloseStart = output.IndexOf(closeDiv);
+output = "Output: " + output.Remove(divCloseStart, closeDiv.Length);
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
 
 #endregion
